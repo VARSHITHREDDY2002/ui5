7ba260a28e22a715d80787c71bd2d0d2fb47f18e
K <varshith.reddy.k@sap.com>
2023-06-28 13:55:05 +0530

    tenth chnage

--- ui5/config.js
+++ ui5/config.js
@@ -16,12 +16,7 @@ exports.config = merge(profile.config, {
   ]
 });
 
-beforeTest: function (test, context) {
-  //console.log('Before Test: ', test.title);
-  console.log('Test file path:', test.file);
-//  console.log('Test description:', test.description);
-
-  // Example: Set up test-specific configurations or variables
-//  context.myVariable = 'Test-specific value';
-// You can access this variable within your test script using `context.myVariable`
+beforeTest: (test, context) => {
+  console.log('Before Test: ', test.title);
+  // Add any code you want to run before each test script execution
 },
